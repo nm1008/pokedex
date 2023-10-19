e45b5bd4dda52f6baf5221d346037d89298c9b0b
nm1008 <mallari08nikko@yahoo.com>
2023-10-19 22:25:30 +0800

    edited cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,20 +1,12 @@
 const PokemonCards = ({ pokemon, index }) => {
-
   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg`;
   const pokeIndex = `#0${index}`;
   const pokeUpperCase = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);
 
   return (
-    <div
-      className="bg-slate-900 border-4 flex flex-col mx-3 my-5 py-2 rounded-xl flex justify-center items-center"
-      style={{}}
-    >
-      <h1 className="text-white flex justify-center my-5 font-bold text-xl">
-        {pokeIndex}
-      </h1>
-      <div className="flex justify-center items-center">
-        <img src={url} alt={pokemon} className="m-5 w-40 h-40" />
-      </div>
+    <div className="bg-slate-900 border-4 flex flex-col mx-3 my-5 py-2 rounded-xl flex justify-center items-center">
+      <h1 className="text-white  my-5 font-bold text-xl">{pokeIndex}</h1>
+      <img src={url} alt={pokemon} className="m-5 w-40 h-40" />
       <h4 className="text-white my-5 font-bold">{pokeUpperCase}</h4>
     </div>
   );
